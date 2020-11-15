0f34d7cdf6bbeba9ad55322728c6980a58870b4d
denis <dediluk@gmail.com>
2020-11-15 17:00:18 +0300

    added index page, social links

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from django.conf.urls import include
 from .views import *
 
+app_name = 'catalog'
+
 urlpatterns = [
-    path('about', aboutMe, name='aboutMe')
+    path('about', aboutMe, name='aboutMe'),
+    path('', index, name='index')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def aboutMe(request):
 	return render(request, 'catalog/aboutMe.html')
+
+
+def index(request):
+	return render(request, 'catalog/index.html')
